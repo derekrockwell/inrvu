460478a1190afced8c484625c8bfc91c39d6a4e3
Derek <derek.a.rockwell@gmail.com>
2013-04-10 14:17:44 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inrvu::Application.config.secret_token = '590fa1407d5b18a845e3a74775759f45a80d6aaa1c2bc4cf9dd7596729ad8369f2012de1fcbc20eaffb5c551b29bc222830ac09fe73fa8092445cae2dfa58e2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Inrvu::Application.config.session_store :cookie_store, key: '_inrvu_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Inrvu::Application.config.session_store :active_record_store
